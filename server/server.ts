99e73fa9f1d0bf584c6f22e97d98265dde313070
SAHIL-SENPAI <sahilyadav3904@gmail.com>
2026-08-14 12:49:14 +0530

    Stripe added

--- server/server.ts
+++ server/server.ts
@@ -14,8 +14,12 @@ import { inngest, functions } from "./inngest/index.js"
 import addressRouter from "./routes/addressRoutes.js";
 import adminRouter from "./routes/adminRoutes.js";
 import deliveryPartnerRouter from "./routes/deliveryPartnerRoutes.js";
+import stripeWebhook from "./controllers/webHooks.js";
+
 
 const app = express();
+
+app.post('/api/stripe', express.raw({type:"application/json"}),stripeWebhook)
 
 // Middleware
 app.use(cors())
